e04eb09c70c6f0bd7814050c82ac31f12c447c5e
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-04-17 10:43:09 +0100

    add when numbers are even simply divide input by 2

--- src/main/scala/Hello.scala
+++
@@ -1,7 +0,0 @@
-object CountOdds extends App {
-
-  def input : Int ={
-    1
-  }
-
-}
---
+++ src/test/scala/CountOddsSpec.scala
@@ -0,0 +1,16 @@
+import org.scalatest._
+
+class CountOddsSpec extends WordSpec with MustMatchers {
+
+  "CountOdds" must {
+
+    "return 1 when given an input of 2" in {
+      CountOdds.counter(2) mustEqual 1
+    }
+
+    "return 4 when given an input of 8" in {
+      CountOdds.counter(8) mustEqual 4
+    }
+  }
+
+}
